9fbd6c24e3ca319803f6477bbe7839cca711a8a9
greybeard <greybeard@gmail.com>
2024-03-26 00:35:27 +0300

    initial commit 2

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Python script that, using this REST API, for a given employee ID
+returns information about his/her TODO list progress.
+"""
+import requests
+import sys
+import csv
+
+
+if __name__ == "__main__":
+
+    todo_url = f'https://jsonplaceholder.typicode.com/user/{sys.argv[1]}/todos'
+    users_url = f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}'
+    todo = requests.get(todo_url).json()
+    users = requests.get(users_url).json()
+
+    with open(f"{sys.argv[1]}.csv", "w", newline='') as f:
+
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+
+        for lst in todo:
+            writer.writerow([sys.argv[1], lst["completed"], lst['title']])
