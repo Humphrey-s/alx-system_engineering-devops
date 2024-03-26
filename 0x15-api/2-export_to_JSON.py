1c0ead6a19fe82cc3e3cc185ed14ef771d805c7d
greybeard <greybeard@gmail.com>
2024-03-26 07:44:53 +0300

    initial commit 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Python script that, using this REST API, for a given employee ID
+returns information about his/her TODO list progress.
+"""
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+
+    todo_url = f'https://jsonplaceholder.typicode.com/user/{sys.argv[1]}/todos'
+    users_url = f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}'
+    todo = requests.get(todo_url).json()
+    users = requests.get(users_url).json()
+
+    key = str(sys.argv[1])
+    dict_lst = []
+    dict_values = {}
+    dic_t = {}
+
+    for lst in todo:
+
+        dic_t['username'] = users["username"]
+        dic_t['task'] = lst['title']
+        dic_t['completed'] = lst['completed']
+
+        dict_lst.append(dic_t)
+        dic_t = {}
+
+    dict_values[key] = dict_lst
+
+    with open(f"{sys.argv[1]}.json", "w") as f:
+
+        json.dump(dict_values, f)
