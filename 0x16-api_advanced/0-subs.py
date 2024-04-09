8dbd1c9cd48cf17eee3257f6e35947a8804b49c5
greybeard <greybeard@gmail.com>
2024-04-09 16:02:23 +0300

    pep8

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,16 +6,13 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+
     if subreddit is None:
         return 0
-
     try:
-        response = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json")
+        url = f"https://www.reddit.com/r/{subreddit}/about.json"
+        response = requests.get(url)
         dct = response.json()
         return dct['data']['subscribers']
     except Exception as e:
-        print(e)
         return 0
-
-if __name__ == '__main__':
-    number_of_subscribers("programming")
