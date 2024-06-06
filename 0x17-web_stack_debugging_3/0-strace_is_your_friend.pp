b489660fb9601a67007c2e6aa846244b54cd26d0
greybeard <greybeard@gmail.com>
2024-06-06 18:13:37 +0300

    indeed

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
-# fix 500 internal error
+# Fixes 500 internal error
 exec { 'fix-wordpress':
-  command     => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path        => ['/bin', '/usr/bin'],
-  logoutput   => true,
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
