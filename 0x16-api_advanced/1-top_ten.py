2f60468f5a7361585e2131030b827f09b76e43d4
greybeard <greybeard@gmail.com>
2024-06-07 01:34:02 +0300

    api

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,14 +6,17 @@ import requests
 def top_ten(subreddit):
     """prints top 10 hot posts of a subreddit"""
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) Apple' +
+        'WebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
+    }
 
-    response = requests.get(url, allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
         print("None")
-        return
+    else:
+        posts = response.json().get("data").get("children")
 
-    posts = response.json().get("data").get("children")
-
-    for post in posts:
-        print(post.get("data")["title"])
+        for post in posts:
+            print(post.get("data")["title"])
