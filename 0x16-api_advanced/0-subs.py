6e4946b3cec6d2dd9966d89a588b3792b53bfe6d
greybeard <greybeard@gmail.com>
2024-06-07 00:06:13 +0300

    requests

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """return number of subcribers in a subreddit"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
 
-    response = requests.get(url)
+    response = requests.get(url, allow_redirects=False)
     if response.status_code != 200:
         return 0
 
