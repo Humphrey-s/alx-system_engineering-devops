c6440380b03922c1c855cb41336e400441abe74c
greybeard <greybeard@gmail.com>
2024-06-13 20:48:13 +0300

    nginx + flask

--- 0x1A-application_server/setup.py
+++ 0x1A-application_server/setup.py
@@ -4,5 +4,5 @@ env.hosts = ["ubuntu@18.207.112.206", "ubuntu@54.89.195.83"]
 
 def do_net():
     """install net tools"""
-    run("sudo apt install net-tools -y")
-    run("sudo git clone https://github.com/Humphrey-s/AirBnB_clone_v2.git")
+    run("sudo apt update")
+    run("sudo reboot")
