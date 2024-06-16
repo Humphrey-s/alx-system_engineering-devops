134ea1929738faaf6ff84ad3ca111874041020b1
greybeard <greybeard@gmail.com>
2024-06-16 21:22:31 +0300

    default

--- 0x1A-application_server/setup.py
+++ 0x1A-application_server/setup.py
@@ -5,4 +5,5 @@ env.hosts = ["ubuntu@18.207.112.206", "ubuntu@54.89.195.83"]
 def do_net():
     """install net tools"""
     run("sudo apt update")
-    run("sudo reboot")
+    #run("sudo git clone https://github.com/Humphrey-s/AirBnB_clone_v3.git")
+    run("sudo apt install mysql-server -y")
