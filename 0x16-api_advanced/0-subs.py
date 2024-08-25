8af3f6d576b3ebadf4366e982f9c909e13924d76
greybeard <greybeard@gmail.com>
2024-08-25 07:55:16 +0300

    disallow redirections

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,8 +5,8 @@ import requests
 
 def number_of_subscribers(subreddit):
     """queries the Reddit API and returns the number of subscribers"""
-
-    r = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json")
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    r = requests.get(url, allow_redirects=False)
 
     if r.status_code != 200:
         return 0
